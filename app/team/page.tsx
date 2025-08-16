35d236d7d16a4c0119713039de3acc471c36b834
OmarSherifOCT <omarsherifrko@gmail.com>
2025-08-16 06:41:53 +0300

    Update page.tsx

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -65,7 +65,7 @@ const teamMembers = [
     name: 'Omar Emam',
     role: 'Chief Operating Officer', 'Chief Product Officer',
     bio: 'Egypt',
-    image: '/images/team/Omar Emam.jpg', // Local image path
+    image: '/images/team/Omar Emam .jpg', // Local image path
     socialLinks: {
       linkedin: 'https://www.linkedin.com/in/omar-emam-b71421306/',
     }
